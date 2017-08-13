2ca5afd66529fb7881c9475c583740195cc88014
Marlon Luft Salvador <marlon-luft-salvador@hotmail.com>
2017-08-12 22:27:59 -0300

    Alterações
    Alterado rota, adicionado logo, prestação de contas

--- Scripts/site.js
+++ Scripts/site.js
@@ -1,7 +1,8 @@
 var scotchApp = angular.module('scotchApp', ['ngRoute']);
 
 // ROTAS
-scotchApp.config(function ($routeProvider, $windowProvider) {
+scotchApp.config(function ($routeProvider, $windowProvider, $locationProvider) 
+{
     $routeProvider
         .when('/', {
             templateUrl: '../Views/Login/index.html',
@@ -26,7 +27,16 @@ scotchApp.config(function ($routeProvider, $windowProvider) {
         .when('/solicitarViagem', {
             templateUrl: '../Views/solicitarViagem/index.html',
             controller: 'colaboradorController'
+        })
+        .when('/naoEncontrado', {
+            templateUrl: '../Views/Error/naoEncontrado.html',
+            controller: 'errorController'
+        })
+        .otherwise({
+            redirectTo: '/naoEncontrado'
         });
+
+    $locationProvider.html5Mode(true);
 
     var $window = $windowProvider.$get();
 });
@@ -34,14 +44,12 @@ scotchApp.config(function ($routeProvider, $windowProvider) {
 // CONTROLLERs
 scotchApp.controller('mainController', function ($scope, $window) {
 
-    var caminho = "http://" + $window.location.host + "/" + "Layout.html#!/";
-    
-    $scope.menuLogin = caminho;
-    $scope.menuGestor = caminho + 'gestor/';
-    $scope.menuSetor = caminho + 'setor/';
-    $scope.menuUsuario = caminho + 'usuario/';
-    $scope.menuColaborador = caminho + 'colaborador/';
-    $scope.menuSolicitarViagem = caminho + 'solicitarViagem/';
+    $scope.menuLogin = "/";
+    $scope.menuGestor = '/gestor/';
+    $scope.menuSetor = '/setor/';
+    $scope.menuUsuario = '/usuario/';
+    $scope.menuColaborador = '/colaborador/';
+    $scope.menuSolicitarViagem = '/solicitarViagem/';
 
 });
 
@@ -52,33 +60,60 @@ scotchApp.controller('loginController', function ($scope, $window) {
         //login.senha
         //login.lembrar
 
-        $scope.Redirect(login.email || "");
-
-        $scope.Redirect = function (modulo) {
-            switch (modulo) {
-
-                case "gestor":
-                    $window.location.href = $window.location.href + 'gestor/';
-                    break;
-
-                case "admin":
-                    $window.location.href = $window.location.href + 'usuario/';
-                    break;
-
-                case "colaborador":
-                default:
-                    $window.location.href = $window.location.href + 'colaborador/';
-                    break;
-            }
-        }
     }
 
 });
 
 scotchApp.controller('colaboradorController', function ($scope) {
 
+    $scope.prestacoesConta = [];
+
+    $scope.categorias = [
+        {text:'Passagem', value: 0},
+        {text:'Hospedagem', value: 1},
+        {text:'Alimentação', value: 2},
+        {text:'Transporte', value: 3},
+        {text:'Outro', value: 4}
+    ];
+
     $scope.SolicitarViagem = function () {
         $('#myModal').modal('show');
+    }
+
+    $scope.adicionarPrestacaoConta = function (prestacao)
+    {
+        var obj = angular.copy(prestacao);
+        obj.id = $scope.prestacoesConta.length;
+
+        $scope.prestacoesConta.push(obj);
+    }
+
+    $scope.recupearDescricao = function(value)
+    {
+        var obj = $scope.categorias[Object.keys($scope.categorias).find(x => $scope.categorias[x].value === value)];
+        return obj.text;
+    }
+
+    $scope.recuperarTotalPrestacao = function()
+    {
+        var total = 0;
+
+        $scope.prestacoesConta.forEach(function(element) {
+            total = total + (parseInt(element.quantidade) * parseFloat(element.valor));
+        }, this);
+
+        return total.toFixed(2);
+    }
+
+    $scope.removerPrestacao = function(id)
+    {
+        for(var i = $scope.prestacoesConta.length -1; i >= 0; i--)
+        {
+            if ($scope.prestacoesConta[i].id == id)
+            {
+                $scope.prestacoesConta.splice(i, 1);
+            }
+        }
     }
 
 });
@@ -102,5 +137,11 @@ scotchApp.controller('usuarioController', function ($scope) {
     $scope.NovoUsuario = function () {
         $('#modalNovo').modal('show');
     }
+
+});
+
+scotchApp.controller('errorController', function ($scope) {
+
+
 
 });
