7280ae9aa6245bda9005aaf95506cc8593b40845
Marlon Luft Salvador <marlon-luft-salvador@hotmail.com>
2017-08-27 00:52:03 -0300

    Esconder menu on click

--- Scripts/site.js
+++ Scripts/site.js
@@ -1,3 +1,9 @@
 $(window).on('load', function () {
     $("body").tooltip({ selector: '[data-toggle=tooltip]', placement: 'bottom' });
 });
+
+$(document).on('click','.navbar-collapse.in',function(e) {
+    if( $(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle' ) {
+        $(this).collapse('hide');
+    }
+});
